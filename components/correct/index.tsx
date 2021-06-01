dd6d7a12ec4f47399117f166b58baefb17d8811d
Shah Kashani <skashani@gmail.com>
2021-06-01 23:31:24 +0200

    Style change

--- components/correct/index.tsx
+++ components/correct/index.tsx
@@ -2,7 +2,7 @@ import styles from './styles.module.scss';
 
 export default function Correct({ onComplete }: { onComplete?: () => void }) {
   if (onComplete) {
-    setTimeout(() => onComplete(), 1000);
+    setTimeout(() => onComplete(), 1500);
   }
   return <div className={styles.container}>🎉</div>;
 }
